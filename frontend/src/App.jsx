175fe819e8927e62d80443b7d562c2e8c096efa3
Yassir Irfan <yassirirfan7@gmail.com>
2023-09-13 14:14:33 +0530

    [ADD] UI Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import UpdateBook from './pages/UpdateBook'
+import DeleteBook from './pages/DeleteBook'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/update/:id' element={<UpdateBook />}/>
+      <Route path='/books/delete/:id' element={<DeleteBook />}/>
+      <Route path='/books/details/:id' element={<ShowBook />}/>
+    </Routes>
   )
 }
 
