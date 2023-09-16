576542b47ac8569cf17656f9685bd243277b4c89
Yassir Irfan <yassirirfan7@gmail.com>
2023-09-16 14:12:44 +0530

    [UPD] seperated table-card components

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineDelete } from "react-icons/md";
 
 function BooksTable({books}) {
   return (
